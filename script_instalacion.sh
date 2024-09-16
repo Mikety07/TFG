0ef0051898b6cab268b91b776cc0a405e33c3f32
Mikety <71652110+Mikety07@users.noreply.github.com>
2024-09-16 12:45:59 +0200

    Delete script_instalacion.sh

--- script_instalacion.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install apache2
-sudo apt install mysql-server
-sudo apt install php libapache2-mod-php php-mysql
-sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl
-sudo phpenmod mbstring
